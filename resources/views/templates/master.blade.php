42d287566301169c8d37dc51daad237aa012c822
Do Viet Hung <patricedo@Dos-MacBook-Pro.local>
2021-07-02 08:40:34 +0700

    update1.2.020721

--- resources/views/templates/master.blade.php
+++ resources/views/templates/master.blade.php
@@ -34,11 +34,7 @@
                             <li class="nav-item">
                                 <a class="nav-link" href="{{ route('login') }}">{{ __('Đăng nhập') }}</a>
                             </li>
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Đăng ký') }}</a>
-                                </li>
-                            @endif
+                          
                         @else
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
